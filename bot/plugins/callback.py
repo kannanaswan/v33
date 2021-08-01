47777304aa6ed629ef4cbe4616e1753b82599977
kannanaswan <88264541+kannanaswan@users.noreply.github.com>
2021-08-01 23:43:15 +0530

    Update callback.py

--- bot/plugins/callback.py
+++ bot/plugins/callback.py
@@ -1680,7 +1680,7 @@ async def callback_data(bot, update: CallbackQuery):
 
     elif query_data == "about": 
         buttons = [[
-            InlineKeyboardButton('👤 @𝙼𝚛𝚔_𝚈𝚃 👤', url='https://t.me/MRK_YT')
+            InlineKeyboardButton('👤 @cybermediaworks 👤', url='https://t.me/Cybermediaworks')
         ],[
             InlineKeyboardButton('𝙼𝚊𝚜𝚝𝚎𝚛 @𝙰𝚕𝚋𝚎𝚛𝚝𝙴𝚒𝚗𝚜𝚝𝚎𝚒𝚗𝚃𝙶', url='https://t.me/AlbertEinsteinTG')
         ],[
